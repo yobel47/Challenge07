979bbbcc5b84c9adc2892413911a47354aebaced
007AxelBerkati <bucel1916@gmail.com>
2022-05-22 22:58:49 +0700

    update logic for get all user

--- src/pages/DashboardScreen/index.js
+++ src/pages/DashboardScreen/index.js
@@ -1,7 +1,9 @@
+/* eslint-disable no-param-reassign */
 import React, { useEffect, useState } from 'react';
 import {
   FlatList, StyleSheet, View,
 } from 'react-native';
+import uuid from 'react-native-uuid';
 import { ILNullPhoto } from '../../assets';
 import { ButtonComponent, Header, List } from '../../component';
 import { databaseRef } from '../../config/Firebase/index.js';
@@ -43,6 +45,43 @@ function DashboardScreen({ navigation }) {
       });
   };
 
+  const createChatList = (data) => {
+    // console.log('data', data);
+    databaseRef()
+      .ref(`/chatlist/${profile.uid}/${data.uid}`)
+      .once('value')
+      .then((snapshot) => {
+        // console.log('User data: ', snapshot.val());
+
+        if (snapshot.val() == null) {
+          const roomId = uuid.v4();
+          const myData = {
+            roomId,
+            uid: profile.uid,
+            fullname: profile.fullname,
+            photo: profile.photo,
+            bio: profile.bio,
+            lastMsg: '',
+          };
+          databaseRef()
+            .ref(`/chatlist/${data.uid}/${profile.uid}`)
+            .update(myData);
+          // .then(() => console.log('Data updated.'));
+
+          data.lastMsg = '';
+          data.roomId = roomId;
+          databaseRef()
+            .ref(`/chatlist/${profile.uid}/${data.uid}`)
+            .update(data);
+          // .then(() => console.log('Data updated.'));
+
+          navigation.navigate('ChatScreen', { receiverData: data, profile });
+        } else {
+          navigation.navigate('ChatScreen', { receiverData: snapshot.val(), profile });
+        }
+      });
+  };
+
   return (
     <View style={styles.page}>
       <Header type="dashboard-profile" title={profile.fullname} photo={profile.photo} onPress={() => navigation.navigate('ProfileScreen', profile)} />
@@ -56,7 +95,7 @@ function DashboardScreen({ navigation }) {
             chat={item.bio}
             profile={item.photo}
             type="next"
-            onPress={() => navigation.navigate('ChatScreen', item)}
+            onPress={() => createChatList(item)}
           />
         )}
       />
